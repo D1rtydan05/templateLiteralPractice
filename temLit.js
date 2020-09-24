c7a0cb99782e9ac7971aefab1ee36b8502faff65
Daniel Garcia <dgarcia300@west-mec.org>
2020-09-23 18:08:40 -0700

    should be correct
    I hope

--- temLit.js
+++ temLit.js
@@ -1,31 +1,49 @@
-// Name:
-// Date:
+// Name: Daniel Garcia
+// Date: 9 - 23 - 2020
 // Assignment: Template Literal Practice
+
+/*
 
 ////////////////////////Pt. 1////////////////////////
 // create a var that accepts a first name as a prompt
-
+    let firstName = prompt("Please enter your first Name");
+    firstName;
 
 // create a var that accepts a last name as a prompt
-
+    let lastName = prompt("Please enter your last Name");
+    lastName;
 
 // Create a variable that will hold a score and a var that will hold a full name 
+    // let number = Math.random(1, 100);
+    let score = 50;
 
+
+    let fullName = `${firstName} ${lastName}`;
+    console.log(fullName);
 
 // create a var that adds the full name var and score in a message
+    let finalMsg = `${fullName} ${score}`;
+    finalMsg;
 
+*/
+
+/*
 
 ////////////////////////Pt. 2////////////////////////
 //comment out the lines above then create two var's that hold numbers
-
+let numOne = 20;
+let numTwo = 30;
 
 //create an add var that adds both vars
-
+let add = `${numOne}  ${numTwo}`;
 
 //console log the newly created add var
-
+console.log(add)
 
 //console log 3 string's using concatination that say "concatinating is easy""when using template literals,""I can even embed numbers and not have to worry about spacing!"
-
+console.log(`concatination is easy when usung template literals, I can even embed numbers and not have to worry about spacing!`);
 
 //console log the three strings above and add a third string that includes the add var
+console.log(`concatination is easy when usung template literals, I can even embed numbers and not have to worry about spacing! ${add}`);
+
+*/
